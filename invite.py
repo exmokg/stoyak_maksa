4a3bdf4b985b548e5e85ed0b5823fbc8a7a6d519
exmokg <87359285+exmokg@users.noreply.github.com>
2024-02-01 19:52:42 +0600

    Update invite.py

--- invite.py
+++ invite.py
@@ -1,21 +1,4 @@
-#!/usr/bin/env python3
-from telethon.sync import TelegramClient
-from telethon.tl.functions.messages import GetDialogsRequest
-from telethon.tl.types import InputPeerEmpty, InputPeerChannel, InputPeerUser
-from telethon.errors.rpcerrorlist import PeerFloodError, UserPrivacyRestrictedError
-from telethon.tl.functions.channels import InviteToChannelRequest
-import configparser
-import os, sys
-import csv
-import traceback
-import time
-import random
-
-re="\033[1;31m"
-gr="\033[1;32m"
-cy="\033[1;36m"
-
-def banner():
+:
     print(f"""
 ░▄▀▀░▀█▀░▄▀▄░▀▄▀▒▄▀▄░█▄▀░░░█▄▒▄█▒▄▀▄░█▄▀░▄▀▀▒▄▀▄
 ▒▄██░▒█▒░▀▄▀░▒█▒░█▀█░█▒█▒░░█▒▀▒█░█▀█░█▒█▒▄██░█▀█
